be523707ab01f9e7ca3465379320439baeb350f8
Jorge Aragon <43160434+jrgaragon@users.noreply.github.com>
2018-09-15 19:37:38 -0700

    nah

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,13 +1,5 @@
-const fs = require('fs');
+const { crearArchivo } = require('./multiplicar/miltiplicar')
 
 let base = 5;
-let data = '';
 
-for (let i = 1; i <= 10; i++) {
-    data += `${base} * ${i} = ${i*base}\n`;
-}
-
-fs.writeFile(`tabla-${base}.txt`, data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
+crearArchivo(base).then(file => console.log(file)).catch(err => { console.log(err) })
